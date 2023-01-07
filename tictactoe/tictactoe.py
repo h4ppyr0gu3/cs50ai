e3aeed130cac0baec691b45b624ce90d0716e1f6
david rogers <rogersdpdr@gmail.com>
2023-01-07 15:05:14 +0100

    wip

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -22,35 +22,61 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    xsum = 0
+    osum = 0
+    for i in board:
+        for j in i:
+            if j == X:
+                xsum += 1
+            if j == O:
+                osum += 1
 
+    if xsum > osum:
+        return O
+    if osum > xsum:
+        return X
+    if osum == xsum:
+        return X
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
 
+    moves = []
+    for i in board:
+        for j in i:
+            if j == None:
+                moves.append((i, j))
+    return moves
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
-
+    board[action[0]][action[1]] = player(board)
+    return board
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    for i in board:
+        if len(list(set(i))) == 1 && list(set(i)) != [None]:
+            return list(set(i))[0]
+        for i in range(0, 3):
+            # last = None
+            # count = 0
+        # for j in i:
 
+    raise NotImplementedError
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    return False
+    # raise NotImplementedError
 
 
 def utility(board):
